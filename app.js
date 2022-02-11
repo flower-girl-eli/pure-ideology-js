156c93f913ac9accd3d57ff76fc4f0018ce7871f
Eliška B <71393504+flower-girl-eli@users.noreply.github.com>
2022-02-11 16:38:16 +0100

    Started writting code, added dependencies

---
+++ app.js
@@ -0,0 +1,9 @@
+const Twitter = require("twitter");
+
+const verification = new Twitter ({
+    consumer_key: "",
+    consumer_secet: "",
+    access_token_key: "",
+    acess_token_secret: ""
+});
+
